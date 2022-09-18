850bba1806b937c5945800a8c42c37628c69775b
RAKESH R <94379161+Rockeypalan@users.noreply.github.com>
2022-09-18 17:19:17 +0530

    heroku

---
+++ client/build/precache-manifest.01d085233cc99f023dec2ea1aedbb5db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331872e4251b5db6e58422c54459ca75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43d1ccd331d6d1f9a22",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43d1ccd331d6d1f9a22",
+    "url": "/static/js/main.7455cc4b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
